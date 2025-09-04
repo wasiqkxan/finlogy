66a47847982ec31e36e8a2f319a7fa01be0362f1
wasiqkhan336 <wasiqkhan336@gmail.com>
2025-09-04 09:47:27 +0000

    seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Account;
+use App\Models\Category;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,7 +15,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(10)->create()->each(function ($user) {
+            Account::factory(3)->create(['user_id' => $user->id]);
+            Category::factory(5)->create(['user_id' => $user->id]);
+        });
 
         User::factory()->create([
             'name' => 'Test User',
